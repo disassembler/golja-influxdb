f954ca115f7d0a81de39251775d0ec46b2bf073e
Samuel Leathers <sam@appliedtrust.com>
2016-05-20 12:39:30 -0400

    custom yum repo for environments without internet access

--- manifests/repo/yum.pp
+++ manifests/repo/yum.pp
@@ -1,17 +1,36 @@
 # PRIVATE CLASS: do not use directly
-class influxdb::repo::yum {
+class influxdb::repo::yum (
+  $custom_repo   = undef,
+  $custom_gpgkey = undef,
+) {
 
   $_operatingsystem = $::operatingsystem ? {
     'CentOS' => downcase($::operatingsystem),
     default  => 'rhel',
   }
 
+  $baseurl = $custom_repo ? {
+    undef   => "https://repos.influxdata.com/${$_operatingsystem}/\$releasever/\$basearch/stable",
+    default => $custom_repo,
+  }
+  $gpgkey = $custom_gpgkey ? {
+    false   => absent,
+    undef   => 'https://repos.influxdata.com/influxdb.key',
+    default => $custom_gpgkey,
+  }
+  if $custom_gpgkey == false {
+    $gpgcheck = 0
+  }
+  else {
+    $gpgcheck = 1
+  }
+
   yumrepo { 'repos.influxdata.com':
     descr    => "InfluxDB Repository - ${::operatingsystem} \$releasever",
-    baseurl  => "https://repos.influxdata.com/${$_operatingsystem}/\$releasever/\$basearch/stable",
+    baseurl  => $baseurl,
     enabled  => 1,
-    gpgcheck => 1,
-    gpgkey   => 'https://repos.influxdata.com/influxdb.key',
+    gpgcheck => $gpgcheck,
+    gpgkey   => $gpgkey,
   }
 
   Yumrepo['repos.influxdata.com'] -> Package<| tag == 'influxdb' |>
